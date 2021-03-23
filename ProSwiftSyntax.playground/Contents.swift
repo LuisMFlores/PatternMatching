f056a0c5ded52c259b15e861dad20f2ea8abb3a6
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-23 19:19:08 -0400

    Validating two values

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -12,3 +12,18 @@ case "twostraws":
 default:
     break
 }
+
+// Validating two values
+
+let newName = "twostraws"
+let password = "fr0st1es"
+
+switch (newName, password) {
+case ("bilbo","bagg1n5"):
+    print("Hello you")
+case ("twostraws", "fr0st1es"):
+    print("Hello Luis M.")
+default:
+    print("Hello Unknown person")
+}
+
