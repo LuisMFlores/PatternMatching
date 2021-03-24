a0acfed6ba009cb9cec0dc224f4698d047c65750
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-24 16:42:21 -0400

    nested functions

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -14,3 +14,57 @@ enum London {
     }
 }
 
+// Nested functions
+
+struct Point {
+    let x: Double
+    let y: Double
+}
+
+enum DistanceTechnique {
+    case euclidean
+    case euclideanSquare
+    case manhattan
+}
+
+func calculateDistance(start: Point, end: Point, technique: DistanceTechnique) -> Double {
+    func calculateEuclideanDistanceSquared() -> Double {
+        let deltaX = start.x - end.x
+        let deltaY = start.y - end.y
+        
+        return deltaX * deltaX + deltaY * deltaY
+    }
+
+    func calculateEuclideanDistance() -> Double {
+        return sqrt(calculateEuclideanDistanceSquared())
+    }
+
+    func calculateManhattanDistance() -> Double {
+        return abs(start.x - end.x) + abs(start.y - end.y)
+    }
+    
+    switch technique {
+    case .euclideanSquare:
+        return calculateEuclideanDistanceSquared()
+    case .euclidean:
+        return calculateEuclideanDistance()
+    case .manhattan:
+        return calculateManhattanDistance()
+    }
+}
+
+
+
+switch technique {
+case <#pattern#>:
+    <#code#>
+default:
+    <#code#>
+}
+
+let start = Point(x: 10, y: 10)
+let end = Point(x: 20, y: 20)
+
+print(calculateEuclideanDistanceSquared(start: start, end: end))
+print(calculateEuclideanDistance(start: start, end: end))
+print(calculateManhattanDistance(start: start, end: end))
