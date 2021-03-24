54b451581744f007a24b70b05fc69e74d2bac5c8
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-24 11:16:17 -0400

    nil coalescing

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -1,4 +1,27 @@
 import UIKit
 
 //runSyntaxChapter()
+//runNilCoalescing()
 
+// Nil Coalescing
+
+var name: String? = "Taylor"
+name = nil
+var unwrappedName = name ?? "Anonymous"
+print(unwrappedName)
+
+print(name ?? "Anonymus")
+
+var savedText: String
+
+do {
+    savedText = try String(contentsOfFile: "saved.txt")
+} catch {
+    print("Failed to fetch this")
+    savedText = "Hello, World"
+}
+
+print(savedText)
+
+savedText = (try? String(contentsOfFile: "saved.txt")) ?? "Hello world"
+print(savedText)
