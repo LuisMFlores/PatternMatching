1e5eda5588fa4347cf63b9bb3d63330d298077a9
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-24 18:44:36 -0400

    Generic struct

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -13,3 +13,33 @@ func square<T: Numeric>(withValue value: T) -> T {
 }
 
 print(square(withValue: 16.5))
+
+// Creating a generic type
+
+struct Deque<T> {
+    var array = [T]()
+    
+    mutating func pushBack(_ obj: T) {
+        array.append(obj)
+    }
+    
+    mutating func pushFront(_ obj: T) {
+        array.insert(obj, at: 0)
+    }
+    
+    mutating func popBack() -> T {
+        return array.removeLast()
+    }
+    
+    mutating func popFront() -> T? {
+        guard !array.isEmpty else { return nil }
+        return array.removeFirst()
+    }
+}
+
+var testDequeue = Deque<Int>()
+testDequeue.pushBack(10)
+testDequeue.pushFront(1)
+print(testDequeue)
+testDequeue.popBack()
+testDequeue.popFront()
