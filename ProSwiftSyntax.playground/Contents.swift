789d00ea4b7b6e842d9e4ff3a2791a0b59bc95b7
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-24 11:33:05 -0400

    lazy loading

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -2,4 +2,24 @@ import UIKit
 
 //runSyntaxChapter()
 
+// Lazy loading
+
+class Singer {
+    let name: String
+    let reversedNameString: String
+    
+    init(name: String) {
+        self.name = name
+        // Slow code since it will be calculate multiple time whether or not this property is used 
+        self.reversedNameString = "\(name.uppercased()) backward is \(String(name.uppercased().reversed()))!"
+    }
+    
+    func reversedName() -> String {
+        return "\(name.uppercased()) backward is \(String(name.uppercased().reversed()))!"
+    }
+}
+
+let taylor = Singer(name: "Taylor Swift")
+print(taylor.reversedName())
+
 
