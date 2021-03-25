d9bfe1e5d027ff84c0b32a456b89fcce0af60ecc
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-24 20:32:28 -0400

    variadic functions

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -1,70 +1,25 @@
 import UIKit
 import Foundation
 
-// Generics
+// Variadic functions
 
-func inspect<T>(withValue value: T) {
-    print("Inspecting  \(type(of:value))")
+// Not variadic
+func notVariadicAdd(numbers: [Int]) -> Int {
+    var total = 0
+    for number in numbers {
+        total += number
+    }
+    return total
 }
 
-// Limiting generics
+notVariadicAdd(numbers: [1,2,3,4,5])
 
-func square<T: Numeric>(withValue value: T) -> T {
-    return value * value
+func variadicAdd(numbers: Int...) -> Int {
+    var total = 0
+    for number in numbers {
+        total += number
+    }
+    return total
 }
 
-print(square(withValue: 16.5))
-
-// Creating a generic type
-
-struct Deque<T> {
-    var array = [T]()
-    
-    mutating func pushBack(_ obj: T) {
-        array.append(obj)
-    }
-    
-    mutating func pushFront(_ obj: T) {
-        array.insert(obj, at: 0)
-    }
-    
-    mutating func popBack() -> T {
-        return array.removeLast()
-    }
-    
-    mutating func popFront() -> T? {
-        guard !array.isEmpty else { return nil }
-        return array.removeFirst()
-    }
-}
-
-var testDequeue = Deque<Int>()
-testDequeue.pushBack(10)
-testDequeue.pushFront(1)
-print(testDequeue)
-testDequeue.popBack()
-testDequeue.popFront()
-
-// Working with cocoa types
-
-struct CustomCountedSet<T:Any> {
-    let internalSet = NSCountedSet()
-    
-    mutating func add(_ object: T) {
-        internalSet.add(object)
-    }
-    
-    mutating func remove(_ object: T) {
-        internalSet.remove(object)
-    }
-    
-    func count(for object: T) -> Int {
-        return internalSet.count(for: object)
-    }
-    
-}
-
-var countedSet = CustomCountedSet<String>()
-countedSet.add("Hello")
-countedSet.add("Hello")
-print(countedSet.count(for: "Hello"))
+variadicAdd(numbers: 1,2,3,4,5)
--- ProSwiftSyntax.playground/Sources/Syntax.swift
+++ ProSwiftSyntax.playground/Sources/Syntax.swift
@@ -545,5 +545,73 @@ public func runSyntaxChapter() {
         }
     }
 
+    // Generics
+
+    func inspect<T>(withValue value: T) {
+        print("Inspecting  \(type(of:value))")
+    }
+
+    // Limiting generics
+
+    func square<T: Numeric>(withValue value: T) -> T {
+        return value * value
+    }
+
+    print(square(withValue: 16.5))
+
+    // Creating a generic type
+
+    struct Deque<T> {
+        var array = [T]()
+        
+        mutating func pushBack(_ obj: T) {
+            array.append(obj)
+        }
+        
+        mutating func pushFront(_ obj: T) {
+            array.insert(obj, at: 0)
+        }
+        
+        mutating func popBack() -> T {
+            return array.removeLast()
+        }
+        
+        mutating func popFront() -> T? {
+            guard !array.isEmpty else { return nil }
+            return array.removeFirst()
+        }
+    }
+
+    var testDequeue = Deque<Int>()
+    testDequeue.pushBack(10)
+    testDequeue.pushFront(1)
+    print(testDequeue)
+    testDequeue.popBack()
+    testDequeue.popFront()
+
+    // Working with cocoa types
+
+    struct CustomCountedSet<T:Any> {
+        let internalSet = NSCountedSet()
+        
+        mutating func add(_ object: T) {
+            internalSet.add(object)
+        }
+        
+        mutating func remove(_ object: T) {
+            internalSet.remove(object)
+        }
+        
+        func count(for object: T) -> Int {
+            return internalSet.count(for: object)
+        }
+        
+    }
+
+    var countedSet = CustomCountedSet<String>()
+    countedSet.add("Hello")
+    countedSet.add("Hello")
+    print(countedSet.count(for: "Hello"))
+
     
 }
