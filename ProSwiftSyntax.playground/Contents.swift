c36ae909183ca8f6fe133d7d4f2d36e139ec20f5
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-26 08:47:56 -0400

    Functional composition

--- ProSwiftSyntax.playground/Contents.swift
+++ ProSwiftSyntax.playground/Contents.swift
@@ -398,3 +398,45 @@ let namesToReverse = ["Bravo", "Paul", "Adele", "Justin"]
 let namesArr = Array(namesToReverse.sorted().reversed())
 print("Names arr is \(namesArr.first)")
 namesArr.map { print($0)}
+
+
+// Functional composition
+
+let london = (name: "London", continent: "Europe", population: 8_539_000)
+let paris = (name: "Paris", continent: "Europe", population: 2_244_000)
+let lisbon = (name: "Lisbon", continent: "Europe", population: 530_000)
+let rome = (name: "Rome", continent: "Europe", population: 2_627_000)
+let tokyo = (name: "Tokyo", continent: "Asia", population: 13_350_000)
+
+let cities = [london, paris, lisbon, rome, tokyo]
+let totalPopulation = cities.reduce(0) { $0 + $1.population}
+let europePopulation = cities.filter { $0.continent == "Europe" }.reduce(0) { $0 + $1.population}
+
+print("The total population is \(totalPopulation) while Europe's population is \(europePopulation)")
+
+precedencegroup CompositionPrecedence {
+    associativity: left
+}
+
+infix operator >>>: CompositionPrecedence
+
+func >>> <T, U, V>(lhs: @escaping (T) -> U, rhs: @escaping (U) -> V) -> (T) -> V {
+    return { rhs(lhs($0)) }
+}
+
+func generateRandomNumber(max: Int) -> Int {
+    let number = Int(arc4random_uniform(UInt32(max)))
+    print("Using number: \(number)")
+    return number
+}
+
+func calculateFactors(number: Int) -> [Int] {
+    return (1...number).filter { number % $0 == 0 }
+}
+
+func reduceToString(numbers: [Int]) -> String {
+    return numbers.reduce("Factors: ") { $0 + String($1) + " " }
+}
+
+let combined = generateRandomNumber >>> calculateFactors >>> reduceToString
+print(combined(10))
